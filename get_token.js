d110679f39e8aff73cc35917291859d596e0d4ab
Dmitry <waild-cat@mail.ru>
2018-05-16 18:39:21 +0300

    Initiated development.

---
+++ get_token.js
@@ -0,0 +1,75 @@
+function login(callback) {
+    var CLIENT_ID = '362df30b7db846dcae9db7be61fad1c3';
+    var REDIRECT_URI = 'https://vk.com';
+
+    function getLoginURL(scopes) {
+        return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
+            '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+            '&scope=' + encodeURIComponent(scopes.join(' ')) +
+            '&response_type=token';
+    }
+
+    var url = getLoginURL([
+        'user-read-email'
+    ]);
+
+    var width = 450,
+        height = 730,
+        left = (screen.width / 2) - (width / 2),
+        top = (screen.height / 2) - (height / 2);
+
+    window.addEventListener("message", function (event) {
+        var hash = JSON.parse(event.data);
+        if (hash.type == 'access_token') {
+            callback(hash.access_token);
+        }
+    }, false);
+
+    var w = window.open(url,
+        'Spotify',
+        'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+    );
+
+    alert('kek');
+
+}
+
+function getUserData(accessToken) {
+    alert('kek');
+    return $.ajax({
+        url: 'https://api.spotify.com/v1/me',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        }
+    });
+}
+
+$(document).ready(function () {
+    $("#submit").on("click", login);
+});
+
+
+function something() {
+    var client_id = '362df30b7db846dcae9db7be61fad1c3',
+        response_type = 'code',
+        redirect_uri = encodeURIComponent('https://vk.com');
+
+    window.location.href ='https://accounts.spotify.com/authorize/?client_id='
+        + client_id + '&response_type=' + response_type
+        + '&redirect_uri=' + redirect_uri;
+    // $.ajax({
+    //     url: "https://accounts.spotify.com/authorize",
+    //     data: {
+    //         client_id: '362df30b7db846dcae9db7be61fad1c3',
+    //         redirect_uri: encodeURIComponent('http://vk.com/'), response_type: 'code'
+    //     },
+    //     type: 'GET',
+    //     cache: false,
+    //     success: function () {
+    //         alert('lol');
+    //     },
+    //     error: function () {
+    //         alert('loh');
+    //     }
+    // });
+}
